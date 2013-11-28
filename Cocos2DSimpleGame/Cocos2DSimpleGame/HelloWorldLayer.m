15c69699a96756f097c41b3266fbb39225469638
Alvaro Crespo <alvaro.andres.crespo@gmail.com>
2013-11-28 20:22:51 -0300

    Fixed issue with enemyCountLabel.

--- Cocos2DSimpleGame/Cocos2DSimpleGame/HelloWorldLayer.m
+++ Cocos2DSimpleGame/Cocos2DSimpleGame/HelloWorldLayer.m
@@ -195,7 +195,11 @@
         _monstersInLevel = _level.enemiesNum;
 
         NSString* enemyCountMessage = [NSString stringWithFormat: @"Enemies killed %d/%d", _monstersDestroyed, _monstersInLevel];
-        [self showLabel:_enemyCountLabel at:ccp(winSize.width/2, winSize.height/2) withMessage:enemyCountMessage fontSize:28];
+        
+        _enemyCountLabel = [CCLabelTTF labelWithString:enemyCountMessage fontName:@"Arial" fontSize:28];
+        _enemyCountLabel.color = ccc3(0,0,0);
+        _enemyCountLabel.position = ccp(winSize.width/2, winSize.height/2);
+        [self addChild:_enemyCountLabel];
         
         NSString* levelMessage = [NSString stringWithFormat: @"Level %d", _level.levelNum];
         _levelLabel = [CCLabelTTF labelWithString:levelMessage fontName:@"Arial" fontSize:28];
@@ -301,13 +305,6 @@
         gunType = Pistol;
     }
     return self;
-}
-
--(void)showLabel:(CCLabelTTF*)label at:(CGPoint)point withMessage:(NSString*)message fontSize:(int)fontSize {
-    label = [CCLabelTTF labelWithString:message fontName:@"Arial" fontSize:28];
-    label.color = ccc3(0,0,0);
-    label.position = point;
-    [self addChild:label];
 }
 
 - (void)unpauseButtonTapped:(id)sender {
